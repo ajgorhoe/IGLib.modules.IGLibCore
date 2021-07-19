d4474ca6537bd1ff0d1ad8a6dff75e7cf8ca94e7
Igor Grešovnik <gresovnik@gmail.com>
2021-07-19 12:24:20 +0200

    Added reflection utilities class.

---
+++ ToArrange/Reflection/UtilReflectionCore.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGLibCore.ToArrange.Reflection
+{
+
+    /// <summary>Some convenient utilities that use reflection.</summary>
+    public static class UtilReflectionCore
+    {
+
+
+        public static Type GetEnumerableType(Type type)
+        {
+            if (type.IsInterface && type.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+                return type.GetGenericArguments()[0];
+            foreach (Type intType in type.GetInterfaces())
+            {
+                if (intType.IsGenericType
+                    && intType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+                {
+                    return intType.GetGenericArguments()[0];
+                }
+            }
+            return null;
+        }
+
+
+        public static bool ISEnumerableOf(Type elementType, Type queriedType, bool considerStringAsEnumerable = false)
+        {
+            Type actualElementType = GetEnumerableType(queriedType);
+            if (actualElementType == null)
+                return false; // not IEnumerable<someType>
+            if (actualElementType == elementType)
+            {
+                if (!considerStringAsEnumerable && typeof(string).IsAssignableFrom(queriedType))
+                {
+                    // the String type implements IEnumerable<char> but we don't consider strings as IEnumerable for the purpose of this method.
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+
+
+    }
+
+
+
+}
