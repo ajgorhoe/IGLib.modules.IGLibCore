0b62f82af5fe7e52fe905b606a4bf12e902fcacf
Igor Grešovnik <gresovnik@gmail.com>
2025-02-25 23:31:04 +0100

    PrintEnv.ps1 - also print current directory content

--- scripts/PrintEnv.ps1
+++ scripts/PrintEnv.ps1
@@ -3,3 +3,7 @@ Write-Output "Current Directory: $(Get-Location)"
 
 # Print all environment variables
 Get-ChildItem Env: | ForEach-Object { Write-Output "$($_.Name)=$($_.Value)" }
+
+# Print the contents of the current directory
+Write-Output "Directory Contents:"
+Get-ChildItem | ForEach-Object { Write-Output $_.FullName }
