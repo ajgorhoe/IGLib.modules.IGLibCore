ea287fcf89e1b759a60d806564a72981b5af0362
Igor Grešovnik <gresovnik@gmail.com>
2025-02-26 02:33:34 +0100

    Updated PrintEnv.ps1, commented testing steps

--- scripts/PrintEnv.ps1
+++ scripts/PrintEnv.ps1
@@ -1,9 +1,11 @@
 # Print the current directory
-Write-Output "Current Directory: $(Get-Location)"
-
-# Print all environment variables
-Get-ChildItem Env: | ForEach-Object { Write-Output "$($_.Name)=$($_.Value)" }
+Write-Output "\nOutput from PrintEnv.ps1:"
+Write-Output "\nCurrent Directory: $(Get-Location)"
 
 # Print the contents of the current directory
-Write-Output "Directory Contents:"
+Write-Output "\n\nCurrent Directory Contents:\n"
 Get-ChildItem | ForEach-Object { Write-Output $_.FullName }
+
+# Print all environment variables
+Write-Output "\n\nEnvironment variables:"
+Get-ChildItem Env: | ForEach-Object { Write-Output "$($_.Name)=$($_.Value)" }
