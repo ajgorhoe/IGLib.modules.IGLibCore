93365e6ffef7e93fca1dbc2da245b0b42276ec39
Igor Grešovnik <gresovnik@gmail.com>
2025-02-26 20:07:14 +0100

    Updated PrintEnv.ps1 (more info about files)

--- scripts/PrintEnv.ps1
+++ scripts/PrintEnv.ps1
@@ -3,8 +3,13 @@ Write-Output "`nOutput from PrintEnv.ps1:"
 Write-Output "`nCurrent Directory: $(Get-Location)"
 
 # Print the contents of the current directory
-Write-Output "`n`nCurrent Directory Contents:`n"
-Get-ChildItem | ForEach-Object { Write-Output $_.FullName }
+Write-Output "`n`nCurrent Directory Contents:"
+Get-ChildItem | ForEach-Object {
+    $name = $_.Name
+    $lastWriteTime = $_.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss")
+    $length = $_.Length
+    Write-Output "$name`t$lastWriteTime`t$length"
+}
 
 # Print all environment variables
 Write-Output "`n`nEnvironment variables:"
