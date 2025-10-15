94540c36d0b46afc590ba771a184568043fd5612
Igor Grešovnik <gresovnik@gmail.com>
2025-10-16 01:35:29 +0200

    Added InitMudulesIGCore project

---
+++ src/0InitModules/InitModulesIGLibCore/InitModulesCore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InitModulesCore
+{
+
+    /// <summary>Dummy class, just to trigger building the initialization project.
+    /// Access is internal in order not to interfere with names of referencing projects.</summary>
+    internal class InitModulesCore
+    {
+
+    }
+}
