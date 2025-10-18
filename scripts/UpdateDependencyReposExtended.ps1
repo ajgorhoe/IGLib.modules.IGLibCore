7fe6a1bd98e92974e89a8099c2d33747b5e7f238
Igor Grešovnik <gresovnik@gmail.com>
2025-10-18 14:16:41 +0200

    Corrected errors due to incorrect script names

--- scripts/UpdateDependencyReposExtended.ps1
+++ scripts/UpdateDependencyReposExtended.ps1
@@ -1,5 +1,5 @@
 
-# Clones or updates the depencencies repositories for IGLibCore.
+# Clones or updates the dependency repositories for IGLibCore.
 Write-Host "`n`nCloning / updating EXTENDED dependency repositories of IGLibCore ..."
 
 # Get the script directory such that relative paths can be resolved:
@@ -10,13 +10,13 @@ $scriptFilename = [System.IO.Path]::GetFileName($scriptPath)
 Write-Host "Script directory: $scriptDir"
 
 # First, call basic update script:
-& $(join-path $scriptDir "UpdateDepencencyRepos.ps1")
+& $(join-path $scriptDir "UpdateDependencyRepos.ps1")
 
 Write-Host "`nUpdating extended dependencies of IGLibCore:`n"
 
 Write-Host "Nothing to do in the extended depenndencies section.`n"
 
-# Already in the basic update script (UpdateDepencencyRepos):
+# Already in the basic update script (UpdateDependencyRepos):
 # Write-Host "`nUpdating IGLibScripts:"
 # & $(Join-Path $scriptDir "UpdateRepo_IGLibScripts.ps1")
 
