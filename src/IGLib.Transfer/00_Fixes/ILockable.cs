28502699ba84453a2b834aba6223a645c59a4158
Igor Grešovnik <gresovnik@gmail.com>
2025-12-26 20:28:19 +0100

    Transferred basic and vector-related numerical utilities from legacy IGLib; errors partially corrected, still 33 compiler errors

---
+++ src/IGLib.Transfer/00_Fixes/ILockable.cs
@@ -0,0 +1,25 @@
+﻿// Copyright © Igor Grešovnik (2008 - present); license:
+// https://github.com/ajgorhoe/IGLib.modules.IGLibCore/blob/main/LICENSE.md
+
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IG.Lib
+{
+
+    /// <summary>Lockable object, has a Lock property that returns a readonly object on which
+    /// lock must be performed in order to lock the object.</summary>
+    public interface ILockable
+    {
+
+        /// <summary>Object used for locking access to internal resources of the current instance of 
+        /// the implementing class, necessary to safely call object's methods from multiple threads.</summary>
+        object Lock { get; }
+    }
+
+}
