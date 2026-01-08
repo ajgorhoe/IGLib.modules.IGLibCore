6a5d50091fa19e6566d6eacea1cac8ed3e84ec9c
Igor Grešovnik <gresovnik@gmail.com>
2026-01-08 11:11:14 +0100

    Added some commands + command descriptions

--- src/IGLib.Core/Commands/SpecificCommands/SampleCommands.cs
+++ src/IGLib.Core/Commands/SpecificCommands/SampleCommands.cs
@@ -15,30 +15,6 @@ using System.Threading.Tasks;
 namespace IGLib.Commands
 {
 
-    /// <summary>This command returns product of its arguments.</summary>
-    public class CommandProduct : GenericCommandBase
-    {
-
-        public CommandProduct(string? description = null, string? descriptionUrl = null) :
-            base((object?[]? parameters) =>
-            {
-                double product = 1.0;
-                if (parameters != null)
-                {
-                    for (int i = 0; i < parameters.Length; ++i)
-                    {
-                        if (parameters[i] is null)
-                            continue;
-                        double? d2 = UtilTypes.ConvertTo<double>(parameters[i], precise: true);
-                        product *= d2 ?? 1.0;
-                    }
-                }
-                return (object?)product;
-            }, description, descriptionUrl)
-        {  }
-
-    }
-
 
     /// <summary>This command returns sum of its arguments.</summary>
     public class CommandSum : GenericCommandBase
@@ -60,7 +36,52 @@ namespace IGLib.Commands
                 }
                 return (object?)sum;
             }, description, descriptionUrl)
-        {  }
+        {
+            if (description == null)
+            {
+                Description = ThisCommandDescription;
+            }
+        }
+
+        public static string ThisCommandDescription =
+              "This command returns sum of its arguments. "
+              + "If no arguments are specified (null or empty array), double 0 is returned. "
+              + "If arguments cannot be converted to type double, exception is thrown.";
+
+    }
+
+
+    /// <summary>This command returns product of its arguments.</summary>
+    public class CommandProduct : GenericCommandBase
+    {
+
+        public CommandProduct(string? description = null, string? descriptionUrl = null) :
+            base((object?[]? parameters) =>
+            {
+                double product = 1.0;
+                if (parameters != null)
+                {
+                    for (int i = 0; i < parameters.Length; ++i)
+                    {
+                        if (parameters[i] is null)
+                            continue;
+                        double? d2 = UtilTypes.ConvertTo<double>(parameters[i], precise: true);
+                        product *= d2 ?? 1.0;
+                    }
+                }
+                return (object?)product;
+            }, description, descriptionUrl)
+        {
+            if (description == null)
+            {
+                Description = ThisCommandDescription;
+            }
+        }
+
+        public static string ThisCommandDescription =
+              "This command returns product of its arguments. "
+              + "If no arguments are specified (null or empty array), double 1 is returned. "
+              + "If arguments cannot be converted to type double, exception is thrown.";
 
     }
 
@@ -90,7 +111,17 @@ namespace IGLib.Commands
                 }
                 return (object?)(sum / numElements);
             }, description, descriptionUrl)
-        {  }
+        {
+            if (description == null)
+            {
+                description = ThisCommandDescription;
+            }
+        }
+
+        public static string ThisCommandDescription =
+            "This command returns average of its arguments. "
+            + "If no arguments are specified (null or empty array), double NaN is returned (not a number). "
+            + "If arguments cannot be converted to type double, exception is thrown.";
 
     }
 
