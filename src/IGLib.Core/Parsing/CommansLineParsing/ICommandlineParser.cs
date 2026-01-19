e9711576e33b8ff3f09606d413b72461eba582bf
Igor Grešovnik <gresovnik@gmail.com>
2026-01-19 15:36:33 +0100

    Command-line parser interface

---
+++ src/IGLib.Core/Parsing/CommansLineParsing/ICommandlineParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace IGLib.Parsing
+{
+    
+    public interface ICommandlineParser
+    {
+
+        string[] CommandlineToArgs(string commandLine);
+
+        string ArgsToCommandline(IEnumerable<string> commandlineArguments);
+
+    }
+
+}
