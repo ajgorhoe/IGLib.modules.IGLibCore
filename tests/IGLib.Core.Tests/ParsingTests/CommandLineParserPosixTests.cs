83d2e20dcd7d157eb4e11e4c328cde0c28f9f60e
Igor Grešovnik <gresovnik@gmail.com>
2026-01-23 11:56:11 +0100

    Posix commandline parser tests use all base methosds to do the job

--- tests/IGLib.Core.Tests/ParsingTests/CommandLineParserPosixTests.cs
+++ tests/IGLib.Core.Tests/ParsingTests/CommandLineParserPosixTests.cs
@@ -36,17 +36,7 @@ namespace IGLib.Commands.Tests
 
         #region CommandLineToArguments
 
-        /// <summary>Tests conversion of command-line to a set of arguments by the current type <see cref="ICommandLineParser"/>,
-        /// which is defined for the current test class by the overridden <see cref="CommandLineParser"/> property. The round-trip
-        /// conversion is checked when applicable.</summary>
-        /// <param name="isRoundTrip">Whether round-trip conversion sgould be checked for the current input. When
-        /// true, the parsed arguments are converted back to command-line string by the current converter, then
-        /// converted to a set of arguments again, after which it is checked that the same arguments are obtained
-        /// as before. Note that conversion back to command-line may not be identical in many cases because variable
-        /// number of whitespace characters between arguments, or different quoting can produce the same results.</param>
-        /// <param name="commandLine">The command line from which arguments are parsed.</param>
-        /// <param name="expectedArgs">The expected arguments that should be obtained by parsing.</param>
-        /// <param name="shouldThrow">Whether parsing should throw an exception.</param>
+        /// <ingeritdoc />
         [Theory]
         // Empty string or null:
         [InlineData(true, "", new string[] { })]
@@ -59,24 +49,12 @@ namespace IGLib.Commands.Tests
         protected override void CommandlineToArgs_RoundTripConversionWorksCorrectly(bool isRoundTrip, string? commandLine,
             string[] expectedArgs, bool shouldThrow = false)
         {
-            Func<ICommandLineParser, string?, string[]> commandLineToArgsFunc
-                = (parser, commandLine) => { return parser.CommandLineToArgs(commandLine!); };
-            CommandlineToArgs_Conversion_TestBase(isRoundTrip, commandLine,
-                expectedArgs, shouldThrow, commandLineToArgsFunc);
+            base.CommandlineToArgs_RoundTripConversionWorksCorrectly(isRoundTrip, commandLine,
+                expectedArgs, shouldThrow);
         }
 
 
-        /// <summary>Tests conversion of command-line to a set of arguments by the current type <see cref="ICommandLineParser"/>,
-        /// which is defined for the current test class by the overridden <see cref="CommandLineParser"/> property. The round-trip
-        /// conversion is checked when applicable.</summary>
-        /// <param name="isRoundTrip">Whether round-trip conversion sgould be checked for the current input. When
-        /// true, the parsed arguments are converted back to command-line string by the current converter, then
-        /// converted to a set of arguments again, after which it is checked that the same arguments are obtained
-        /// as before. Note that conversion back to command-line may not be identical in many cases because variable
-        /// number of whitespace characters between arguments, or different quoting can produce the same results.</param>
-        /// <param name="commandLine">The command line from which arguments are parsed.</param>
-        /// <param name="expectedArgs">The expected arguments that should be obtained by parsing.</param>
-        /// <param name="shouldThrow">Whether parsing should throw an exception.</param>
+        /// <inheritdoc />
         [Theory]
         // Empty string or null:
         [InlineData(true, "", new string[] { })]
@@ -89,22 +67,8 @@ namespace IGLib.Commands.Tests
         protected override void CommandlineToArgs_RoundTripConversionWitSecondOverloadWorksCorrectly(bool isRoundTrip, string? commandLine,
             string[] expectedArgs, bool shouldThrow = false)
         {
-            Func<ICommandLineParser, string?, string[]> commandLineToArgsFunc
-                = (parser, commandLine) => {
-
-                    // int CommandLineToArgs(ReadOnlySpan<char> commandLine, List<string> destination);
-                    if (commandLine == null)
-                    {
-                        return Array.Empty<string>();
-                    }
-                    ReadOnlySpan<char> commandLineSpan = commandLine != null ? commandLine.AsSpan() : ReadOnlySpan<char>.Empty;
-                    List<string> destination = new List<string>();
-                    int numArgs = parser.CommandLineToArgs(commandLineSpan, destination);
-                    string[] argsArray = destination.ToArray();
-                    return argsArray;
-                };
-            CommandlineToArgs_Conversion_TestBase(isRoundTrip, commandLine,
-                expectedArgs, shouldThrow, commandLineToArgsFunc);
+            base.CommandlineToArgs_RoundTripConversionWitSecondOverloadWorksCorrectly(isRoundTrip, commandLine,
+                expectedArgs, shouldThrow);
         }
 
 
