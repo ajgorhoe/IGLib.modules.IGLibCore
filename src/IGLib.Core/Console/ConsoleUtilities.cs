0e22c187b6877329ba42442f0e4d84756b7ef0f2
Igor Grešovnik <gresovnik@gmail.com>
2026-01-29 23:07:46 +0100

    Utility for reading passwords from console

---
+++ src/IGLib.Core/Console/ConsoleUtilities.cs
@@ -0,0 +1,56 @@
+
+
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+
+
+namespace IGLib.ConsoleUtils
+{
+
+    /// <summary>Various utilities that work with console abstractions (interfaces like <see cref="IConsole"/>,
+    /// <see cref="IConsoleKeyInput"/>, <see cref="IConsoleStreams"/>, <see cref="IConsoleStreams"/>, etc.).</summary>
+    public static class ConsoleUtilities
+    {
+
+
+        #region PasswordUtilities
+
+
+        public static string ReadPassword()
+        {
+            var password = new StringBuilder();
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey(true);
+
+                // Handle Backspace
+                if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    password.Remove(password.Length - 1, 1);
+                    Console.Write("\b \b"); // Move back, overwrite with space, move back again
+                }
+                // Ignore other control keys (like Tab or Escape)
+                else if (!char.IsControl(key.KeyChar))
+                {
+                    password.Append(key.KeyChar);
+                    Console.Write("*");
+                }
+            } while (key.Key != ConsoleKey.Enter);
+
+            Console.WriteLine(); // Move to the next line after Enter
+            return password.ToString();
+        }
+
+
+
+        #endregion PasswordUtilities
+
+
+    }
+
+}
