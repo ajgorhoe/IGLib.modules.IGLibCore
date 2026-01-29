aa9ecb24a7cd8cc39fe4bc3e6a17e462da10c466
Igor Grešovnik <gresovnik@gmail.com>
2026-01-29 17:07:30 +0100

    Added SystemConsole adapter class (forwards operations to System.Console)

---
+++ src/IGLib.Core/Console/Abstractions/SystemConsole.cs
@@ -0,0 +1,30 @@
+
+
+using System;
+using System.IO;
+
+namespace IGLib.ConsoleUtils
+{
+
+    public sealed class SystemConsole : IFullConsole
+    {
+
+        public string? ReadLine() => Console.ReadLine();
+
+        public void Write(string? value) => Console.Write(value);
+        public void WriteLine(string? value = null) => Console.WriteLine(value);
+
+        public void Clear() => Console.Clear();
+        public void SetCursorPosition(int left, int top) => Console.SetCursorPosition(left, top);
+        public int CursorLeft { get => Console.CursorLeft; set => Console.CursorLeft = value; }
+        public int CursorTop { get => Console.CursorTop; set => Console.CursorTop = value; }
+
+        public ConsoleKeyInfo ReadKey(bool intercept = false) => Console.ReadKey(intercept);
+        public bool KeyAvailable => Console.KeyAvailable;
+
+        public ConsoleColor ForegroundColor { get => Console.ForegroundColor; set => Console.ForegroundColor = value; }
+        public ConsoleColor BackgroundColor { get => Console.BackgroundColor; set => Console.BackgroundColor = value; }
+        public void ResetColor() => Console.ResetColor();
+    }
+
+}
