d5fcf5b0e98ec0d425ec77efbef31b913363be4a
Igor Grešovnik <gresovnik@gmail.com>
2026-01-29 15:50:45 +0100

    Added documentation comments

--- src/IGLib.Core/Console/ConsoleAbstractions/IConsoleKeyInput.cs
+++ src/IGLib.Core/Console/ConsoleAbstractions/IConsoleKeyInput.cs
@@ -5,14 +5,23 @@ using System;
 namespace IGLib.ConsoleUtils
 {
 
+    /// <summary>A capability interface that supplements the <see cref="IConsole"/> interface with
+    /// keyboard input capability.</summary>
     public interface IConsoleKeyInput
     {
 
-        public interface IConsoleKeyInput
-        {
-            ConsoleKeyInfo ReadKey(bool intercept = false);
+        /// <summary>Waits for console keyboard input, obtains and returns information on the key pressed.</summary>
+        /// <param name="intercept">If true then the key pressed is intercepted and is NOT displayed on the console.
+        /// Default is false (keys that are read are also displayed in console).</param>
+        /// <returns>An object that describes the ConsoleKey constant and Unicode character, if any, that correspond to the 
+        /// pressed console key. The returned <see cref="ConsoleKeyInfo"/> object also describes, in a bitwise combination of 
+        /// <see cref="ConsoleModifiers"/> values in the <see cref="ConsoleKeyInfo.Modifiers"/> property, whether one or more 
+        /// Shift, Alt, or Ctrl modifier keys was pressed simultaneously with the console key.</returns>
+        ConsoleKeyInfo ReadKey(bool intercept = false);
 
-            bool KeyAvailable { get; }
+        /// <summary>Gets a value indicating whether a key press is available in the console input. This check is often 
+        /// performed before <see cref="ReadKey(bool)"/> is performed.</summary>        
+        bool KeyAvailable { get; }
 
         }
     }
