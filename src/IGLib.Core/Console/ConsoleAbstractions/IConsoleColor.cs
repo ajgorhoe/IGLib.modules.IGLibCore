a3ded08278282398dbba82411d65cd1429741d58
Igor Grešovnik <gresovnik@gmail.com>
2026-01-29 16:12:40 +0100

    Added console color capability interface

---
+++ src/IGLib.Core/Console/ConsoleAbstractions/IConsoleColor.cs
@@ -0,0 +1,23 @@
+
+using System;
+
+namespace IGLib.ConsoleUtils
+{
+
+    public interface IConsoleColor
+    {
+
+        /// <summary>Gets or sets the foreground color of the console.</summary>
+        ConsoleColor ForegroundColor { get; set; }
+
+        /// <summary>Gets or sets the background color of the console.</summary>
+        ConsoleColor BackgroundColor { get; set; }
+
+
+        /// <summary>Sets the current console's foreground and background colors to their defaults.</summary>
+        void ResetColor();
+
+
+    }
+
+}
