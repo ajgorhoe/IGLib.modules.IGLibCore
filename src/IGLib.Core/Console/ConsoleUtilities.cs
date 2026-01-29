6297a69d4e3f54a76c92ccb02e92d3e07955e7f1
Igor Grešovnik <gresovnik@gmail.com>
2026-01-29 23:44:42 +0100

    Added pwd reader that returns a string builder

--- src/IGLib.Core/Console/ConsoleUtilities.cs
+++ src/IGLib.Core/Console/ConsoleUtilities.cs
@@ -19,7 +19,77 @@ namespace IGLib.ConsoleUtils
         #region PasswordUtilities
 
 
-        public static StringBuilder ReadPassword(char displayChar = '*', bool repeatForValidation = true, 
+
+
+        public static string ReadPassword(char displayChar = '*', bool repeatForValidation = true,
+            string insertionPrompt = "Insert the password: ",
+            string validationPrompt = "Insert the password again: ")
+        {
+            var password = new StringBuilder();
+            ConsoleKeyInfo key;
+
+            if (string.IsNullOrEmpty(insertionPrompt))
+            {
+                insertionPrompt = "Insert the password: ";
+            }
+            if (string.IsNullOrEmpty(validationPrompt))
+            {
+                validationPrompt = "Insert the password again: ";
+            }
+            Console.Write(insertionPrompt);
+            do
+            {
+                key = Console.ReadKey(true);
+
+                // Handle Backspace
+                if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    password.Remove(password.Length - 1, 1);
+                    Console.Write("\b \b"); // move back, overwrite with space, move back again
+                }
+                else if (!char.IsControl(key.KeyChar)) // ignore other control keys (like Tab or Escape)
+                {
+                    password.Append(key.KeyChar);
+                    Console.Write(displayChar);
+                }
+            } while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine(); // move to the next line after Enter
+            if (repeatForValidation)
+            {
+                var validationPassword = new StringBuilder();
+                while (validationPassword != password)
+                {
+                    validationPassword.Clear();
+                    Console.Write(validationPrompt);
+                    do
+                    {
+                        key = Console.ReadKey(true);
+
+                        // Handle Backspace
+                        if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                        {
+                            password.Remove(password.Length - 1, 1);
+                            Console.Write("\b \b"); // move back, overwrite with space, move back again
+                        }
+                        else if (!char.IsControl(key.KeyChar)) // ignore other control keys (like Tab or Escape)
+                        {
+                            password.Append(key.KeyChar);
+                            Console.Write(displayChar);
+                        }
+                    } while (key.Key != ConsoleKey.Enter);
+                    Console.WriteLine(); // move to the next line after Enter
+
+                }
+            }
+
+            return password.ToString();
+        }
+
+
+
+
+
+        public static StringBuilder ReadPasswordStringBuilder(char displayChar = '*', bool repeatForValidation = true, 
             string insertionPrompt = "Insert the password: ", 
             string validationPrompt = "Insert the password again: ")
         {
