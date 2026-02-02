3af07d2e43018d050ef6d140f936aa1757403287
Igor Grešovnik <gresovnik@gmail.com>
2026-02-02 15:47:14 +0100

    Added a class for global defaults and constants

---
+++ src/IGLib.Core/Globals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace IGLib
+{
+
+    public static class Global
+    {
+
+        /// <summary>Specifies the default <see cref="IFormatProvider"/> to be used in various APIs, such as
+        /// console utilities for reading numbers and boolean values. (see e.g. class 
+        /// <see cref="IGLib.ConsoleUtils.ConsoleUtilities"/>).</summary>
+        public static IFormatProvider DefaultFormatProvider { get; internal set; } 
+            = CultureInfo.InvariantCulture;
+
+
+    }
+
+
+}
