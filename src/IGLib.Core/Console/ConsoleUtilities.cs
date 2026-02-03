4a3a8f3b1de00c03646457ef7e46f52031dc4695
Igor Grešovnik <gresovnik@gmail.com>
2026-02-03 03:14:11 +0100

    Rename, type consraints

--- src/IGLib.Core/Console/ConsoleUtilities.cs
+++ src/IGLib.Core/Console/ConsoleUtilities.cs
@@ -283,22 +283,22 @@ namespace IGLib.ConsoleUtils
 
 
         /// <summary>Parses the specified string by the appropriate TryParse method (such as <see cref="double.TryParse(string?, NumberStyles, IFormatProvider?, out double))
-        /// selected accoding to the actual type of the <typeparamref name="NumericType"/> type parameter. If successful, 
+        /// selected accoding to the actual type of the <typeparamref name="BasicType"/> type parameter. If successful, 
         /// it returns true and assigns the parsed numerical value to <paramref name="valueVariable"/>. 
         /// <para>Warning: Even if parsing is not successful, <paramref name="valueVariable"/> may change.</para></summary>
-        /// <typeparam name="NumericType">Type of the value that is parsed from string and assigned to <paramref name="valueVariable"/>.</typeparam>
+        /// <typeparam name="BasicType">Type of the value that is parsed from string and assigned to <paramref name="valueVariable"/>.</typeparam>
         /// <param name="str">String that is parsed.</param>
         /// <param name="valueVariable">Referene (variable) that the parsed value is assigned to.</param>
         /// <param name="formatProvider">The <see cref="IFormatProvider"/> used for parsing.</param>
         /// <returns>True if parsing was successful (i.e., the provided <paramref name="str"/> actually corresponds to
-        /// a numeric value of type <typeparamref name="NumericType"/>), false if not.</returns>
+        /// a numeric value of type <typeparamref name="BasicType"/>), false if not.</returns>
         /// <exception cref="ArgumentException">When parsing is not implemented for the specified type.</exception>
-        public static bool TryParse<NumericType>(string str, out NumericType valueVariable, IFormatProvider? formatProvider = null)
-            where NumericType : struct  // , IConvertible
+        public static bool TryParse<BasicType>(string str, out BasicType valueVariable, IFormatProvider? formatProvider = null)
+            where BasicType : struct  // , IConvertible
         {
             //value = default;
             //return false;
-            Type valueType = typeof(NumericType);
+            Type valueType = typeof(BasicType);
             switch (Type.GetTypeCode(valueType))
             {
 
@@ -307,56 +307,56 @@ namespace IGLib.ConsoleUtils
                     {
                         Byte temp;
                         bool result = Byte.TryParse(str, NumberStyles.Integer, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                 case TypeCode.SByte:  // sbyte
                     {
                         SByte temp;
                         bool result = SByte.TryParse(str, NumberStyles.Integer, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                 case TypeCode.Int16:  // short
                     {
                         Int16 temp;
                         bool result = Int16.TryParse(str, NumberStyles.Integer, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                 case TypeCode.UInt16:  // ushort
                     {
                         UInt16 temp;
                         bool result = UInt16.TryParse(str, NumberStyles.Integer, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                 case TypeCode.Int32:  // int
                     {
                         Int32 temp;
                         bool result = Int32.TryParse(str, NumberStyles.Integer, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                 case TypeCode.UInt32:  // uint
                     {
                         UInt32 temp;
                         bool result = UInt32.TryParse(str, NumberStyles.Integer, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                 case TypeCode.Int64:  // long
                     {
                         Int64 temp;
                         bool result = Int64.TryParse(str, NumberStyles.Integer, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                 case TypeCode.UInt64:  // uint64
                     {
                         UInt64 temp;
                         bool result = UInt64.TryParse(str, NumberStyles.Integer, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
 
@@ -365,21 +365,21 @@ namespace IGLib.ConsoleUtils
                     {
                         Double temp;
                         bool result = Double.TryParse(str, NumberStyles.Number, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                 case TypeCode.Single:  // float
                     {
                         Single temp;
                         bool result = Single.TryParse(str, NumberStyles.Number, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                 case TypeCode.Decimal:  // decimal
                     {
                         Decimal temp;
                         bool result = Decimal.TryParse(str, NumberStyles.Number, formatProvider, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
 
@@ -389,7 +389,7 @@ namespace IGLib.ConsoleUtils
                     {
                         Char temp;
                         bool result = Char.TryParse(str, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                  case TypeCode.Boolean:  // bool
@@ -419,7 +419,7 @@ namespace IGLib.ConsoleUtils
                                 }
                             }
                         }
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
 
@@ -429,7 +429,7 @@ namespace IGLib.ConsoleUtils
                         DateTime temp;
                         bool result = DateTime.TryParse(str, formatProvider,
                             DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AssumeUniversal, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
                 default:
@@ -443,23 +443,40 @@ namespace IGLib.ConsoleUtils
                         DateTimeOffset temp;
                         bool result = DateTimeOffset.TryParse(str, formatProvider,
                             DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AssumeUniversal, out temp);
-                        valueVariable = (NumericType)(object)temp;
+                        valueVariable = (BasicType)(object)temp;
                         return result;
                     }
 #if NET8_0_OR_GREATER
+                case Type t when t == typeof(TimeOnly):
+                    {
+                        TimeOnly temp;
+                        bool result = TimeOnly.TryParse(str, formatProvider,
+                            DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AssumeUniversal, out temp);
+                        valueVariable = (BasicType)(object)temp;
+                        return result;
+                    }
+                case Type t when t == typeof(DateOnly):
+                    {
+                        DateOnly temp;
+                        bool result = DateOnly.TryParse(str, formatProvider,
+                            DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AssumeUniversal, out temp);
+                        valueVariable = (BasicType)(object)temp;
+                        return result;
+                    }
+
                 // Pointer types:
                 case Type t when t == typeof(IntPtr):
                 {
                     IntPtr temp;
                     bool result = IntPtr.TryParse(str, formatProvider, out temp);
-                    valueVariable = (NumericType)(object)temp;
+                    valueVariable = (BasicType)(object)temp;
                     return result;
                 }
                 case Type t when t == typeof(UIntPtr):
                 {
                         UIntPtr temp;
                     bool result = UIntPtr.TryParse(str, formatProvider, out temp);
-                    valueVariable = (NumericType)(object)temp;
+                    valueVariable = (BasicType)(object)temp;
                     return result;
                 }
 #endif
