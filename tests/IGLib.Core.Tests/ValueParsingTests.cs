256ca76024203b306783934122cbb94775d7c56f
Igor Grešovnik <gresovnik@gmail.com>
2026-02-04 19:07:06 +0100

    Added reduced test for parsing DateTimeOffset

--- tests/IGLib.Core.Tests/ValueParsingTests.cs
+++ tests/IGLib.Core.Tests/ValueParsingTests.cs
@@ -446,10 +446,18 @@ namespace IGLib.Tests
 
         // PARSING DATETIMEOFFSET:
 
+        protected void TryParseGeneric_OfDateTimeOffset_Reduced_WorksCorrectly(string? parsedString,
+            bool expectedSuccess, string? cultureKey = null, bool skipValueVerification = false)
+        {
+            DateTime expectedResult = DateTime.Now; // dummy value
+            TryParse_WorksCorrectly_Base<DateTime>(parsedString, expectedSuccess, expectedResult, cultureKey, skipValueVerification);
+        }
+
+
         //[Theory]
         //[InlineData("2024-01-01T12:00:00+02:00", true)]
         //[InlineData("invalid", false)]
-        protected void TryParseGeneric_OfDateTimeOffset_WorksCorrectly(string? parsedString, bool expectedSuccess)
+        protected void TryParseGeneric_OfDateTimeOffset_1_WorksCorrectly(string? parsedString, bool expectedSuccess)
         {
             DateTimeOffset dummy = default;
             // TryParse_WorksCorrectly_Base<DateTimeOffset>(parsedString, expectedSuccess, dummy);
