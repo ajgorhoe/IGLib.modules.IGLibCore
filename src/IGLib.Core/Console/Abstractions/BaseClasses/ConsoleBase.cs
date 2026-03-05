5556394340d8fd9311c3db316fab403b74cc5891
Igor Grešovnik <gresovnik@gmail.com>
2026-03-05 01:08:33 +0100

    ConsoleBase abstract class

---
+++ src/IGLib.Core/Console/Abstractions/BaseClasses/ConsoleBase.cs
@@ -0,0 +1,13 @@
+﻿namespace IGLib.ConsoleAbstractions;
+
+public abstract class ConsoleBase : IConsole
+{
+    public abstract string? ReadLine();
+
+    // Core output primitives
+    public abstract void Write(string? value);
+    public abstract void WriteLine(string? value = null);
+
+    // Optional: helpers you want all implementations to inherit
+    protected static string NormalizeWrite(string? value) => value ?? string.Empty;
+}
