261fad074195c030602fe74c545ebd4796b2477c
Igor Grešovnik <gresovnik@gmail.com>
2026-03-05 16:24:48 +0100

    Keys queue

--- src/IGLib.Core/Console/Abstractions/Testing/FakeConsole.cs
+++ src/IGLib.Core/Console/Abstractions/Testing/FakeConsole.cs
@@ -17,5 +17,23 @@ public sealed class FakeConsole // : ConsoleBase, IConsoleKeyInput
     private readonly Queue<string?> _lines = new();
     private readonly Queue<ConsoleKeyInfo> _keys = new();
 
+    private readonly StringBuilder _currentLine = new();
+    private readonly IConsole? _loggingConsole;
+
+    public FakeConsole(IConsole? tee = null)
+    {
+        _loggingConsole = tee;
+    }
+
+    public List<ConsoleEvent> Events { get; } = new();
+
+    public string OutputText { get; private set; } = "";
+
+    public void EnqueueLine(string? line) => _lines.Enqueue(line);
+    public void EnqueueKeys(IEnumerable<ConsoleKeyInfo> keys)
+    {
+        foreach (var k in keys) _keys.Enqueue(k);
+    }
+
 }
 
