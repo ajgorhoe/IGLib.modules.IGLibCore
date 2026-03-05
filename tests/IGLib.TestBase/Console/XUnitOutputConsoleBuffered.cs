cfb9fb2fa93f7fe9ec95a00a3ddaee44242792c3
Igor Grešovnik <gresovnik@gmail.com>
2026-03-05 03:02:15 +0100

    Supplemented code documentation

--- tests/IGLib.TestBase/Console/XUnitOutputConsoleBuffered.cs
+++ tests/IGLib.TestBase/Console/XUnitOutputConsoleBuffered.cs
@@ -4,6 +4,20 @@ using Xunit.Abstractions;
 
 namespace IGLib.ConsoleAbstractions.Testing;
 
+/// <summary>An <see cref="IConsole"/> implementation that can be used in xUnit tests.
+/// <para>This console can use line buffered output mode in order to ensure the correct behavior of the 
+/// <see cref="Write(string?)"/> method. The output <see cref="Write(string?)"/> is buffered (accumulated) 
+/// and is not flushed to the test's standard output until WriteLine() is called. The <see cref="IsLineBuffered"/>
+/// property defines whether the output is line buffered. It can be set via optional constructor parameter, or
+/// explicitly after construction.</para>
+/// <para>Alternatively, output is not line buffered. In this case the <see cref="Write(string?)"/> method 
+/// appends a character that indicates that the following line should be part of the current line. This creates
+/// an awckward output, but the advantage is that the <see cref="Write(string?)"/> method writes to standard
+/// output immediately, and output cannot be skipped because there was no <see cref="WriteLine(string?)"/>
+/// called after <see cref="Write(string?)"/>.</para>
+/// <para>If output is line buffered, one can use the <see cref="Flush"/> method or an empty <see cref="WriteLine(string?)"/>
+/// at the end of a test or in finally block, in orderr to ensure that all outputts of <see cref="Write(string?)"/>
+/// are visible in test's output.</para></summary>
 public sealed class XUnitOutputConsole : IConsole
 {
 
@@ -16,7 +30,6 @@ public sealed class XUnitOutputConsole : IConsole
     private readonly ITestOutputHelper _output;
     private readonly StringBuilder _buffer = new();
     private const string LineContinuationCharacter = "⏎"; // U+23CE, "Return Symbol"
-
 
     public bool IsLineBuffered { get; set; } = true;
     
