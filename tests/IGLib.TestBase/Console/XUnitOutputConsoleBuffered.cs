b51395ea6537fd248d2415d7b5e491aa94569889
Igor Grešovnik <gresovnik@gmail.com>
2026-03-05 02:24:21 +0100

    Added a buffered console for XUnit tests

---
+++ tests/IGLib.TestBase/Console/XUnitOutputConsoleBuffered.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace IGLib.ConsoleAbstractions.Testing;
+
+public sealed class XUnitOutputConsole : ConsoleBase
+{
+    private readonly ITestOutputHelper _output;
+    private readonly StringBuilder _buffer = new();
+
+    public XUnitOutputConsole(ITestOutputHelper output) => _output = output;
+
+    public override string? ReadLine()
+        => throw new NotSupportedException("No input support.");
+
+    public override void Write(string? value)
+        => _buffer.Append(value);
+
+    public override void WriteLine(string? value = null)
+    {
+        if (value is not null) _buffer.Append(value);
+        _output.WriteLine(_buffer.ToString());
+        _buffer.Clear();
+    }
+
+    // Optional: flush on dispose / final assertion
+    public void Flush()
+    {
+        if (_buffer.Length > 0)
+        {
+            _output.WriteLine(_buffer.ToString());
+            _buffer.Clear();
+        }
+    }
+}
