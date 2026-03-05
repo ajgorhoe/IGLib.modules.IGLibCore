9f954b7199209cef6214be6d9aa3a7134e01a251
Igor Grešovnik <gresovnik@gmail.com>
2026-03-05 16:20:31 +0100

    FakeConsole.cs added, with event record definitions

---
+++ src/IGLib.Core/Console/Abstractions/Testing/FakeConsole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGLib.ConsoleAbstractions.Testing;
+
+public abstract record ConsoleEvent;
+public sealed record WriteEvent(string Text) : ConsoleEvent;
+public sealed record WriteLineEvent(string Text) : ConsoleEvent;
+public sealed record ReadLineEvent(string? Returned) : ConsoleEvent;
+public sealed record ReadKeyEvent(ConsoleKeyInfo Returned, bool Intercept) : ConsoleEvent;
+
+
