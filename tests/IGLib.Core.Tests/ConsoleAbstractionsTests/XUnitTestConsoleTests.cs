fa11f7f09f5642b7fd9d1020118b5fda1d732c67
Igor Grešovnik <gresovnik@gmail.com>
2026-03-07 17:45:45 +0100

    Added test class fo testing the refactored Console property in TestBase

---
+++ tests/IGLib.Core.Tests/ConsoleAbstractionsTests/XUnitTestConsoleTests.cs
@@ -0,0 +1,67 @@
+﻿
+#nullable disable
+
+using Xunit;
+using FluentAssertions;
+using Xunit.Abstractions;
+using System;
+using System.Collections.Generic;
+using IGLib.Tests.Base;
+using IGLib.Commands.Tests;
+using System.Text;
+using System.Linq;
+
+using IGLib.ConsoleAbstractions;
+
+namespace IGLib.Commands.Tests
+{
+
+    /// <summary>Tests for generic commands (implementations of <see cref="IGenericCommand"/>).</summary>
+    public class XUnitTestConsoleTests : TestBase<XUnitTestConsoleTests>
+    {
+
+
+        /// <summary>This constructor, when called by the test framework, will bring in an object 
+        /// of type <see cref="ITestOutputHelper"/>, which will be used to write on the tests' output,
+        /// accessed through the base class's property <see cref="Output"/> and the property <see cref="TestBase{TestClassType}.Console"/> of type <see cref="IConsole"/>.</summary>
+        /// <param name=""></param>
+        public XUnitTestConsoleTests(ITestOutputHelper output) :  base(output)  // calls base class's constructor
+        {
+            // Remark: the base constructor will assign output parameter to the Output and Console property.
+        }
+
+
+        #region CommandCreation TO REMOVE
+
+        [Fact]
+        protected void CommandCreation_CommandsHaveDistinctIDsGrowingWithCreationTime()
+        {
+            // Arrange:
+            Console.WriteLine("Testing that created commands have distinct IDs and that IDs grow with time of creation:\n");
+            // Act:
+            IGenericCommand cmd1 = new TestCmd1();
+            Console.WriteLine($"Created command {nameof(cmd1)} of type {cmd1?.GetType()?.Name}, ID = {cmd1?.Id}");
+            IGenericCommand cmd2 = new TestCmd2();
+            Console.WriteLine($"Created command {nameof(cmd2)} of type {cmd2?.GetType()?.Name}, ID = {cmd2?.Id}");
+            IGenericCommand cmd3 = new TestCmd3();
+            Console.WriteLine($"Created command {nameof(cmd3)} of type {cmd3?.GetType()?.Name}, ID = {cmd3?.Id}");
+            cmd1.Should().NotBeNull(because: $"PRECOND: It must be able to create a command of type {nameof(TestCmd1)}");
+            cmd2.Should().NotBeNull(because: $"PRECOND: It must be able to create a command of type {nameof(TestCmd2)}");
+            cmd3.Should().NotBeNull(because: $"PRECOND: It must be able to create a command of type {nameof(TestCmd3)}");
+            // Assert:
+            cmd1.Id.Should().NotBe(cmd2.Id, because: "cmd1 and cmd2 are different instances and must have different IDs");
+            cmd1.Id.Should().NotBe(cmd3.Id, because: "cmd1 and cmd3 are different instances and must have different IDs");
+            cmd2.Id.Should().NotBe(cmd3.Id, because: "cmd2 and cmd3 are different instances and must have different IDs");
+            cmd2.Id.Should().BeGreaterThan(cmd1.Id, because: "cmd2 was created later than cmd1 and should have greater ID.");
+            cmd3.Id.Should().BeGreaterThan(cmd2.Id, because: "cmd2 was created later than cmd1 and should have greater ID.");
+        }
+
+        #endregion CommandCreation
+
+
+    }
+
+
+
+}
+
