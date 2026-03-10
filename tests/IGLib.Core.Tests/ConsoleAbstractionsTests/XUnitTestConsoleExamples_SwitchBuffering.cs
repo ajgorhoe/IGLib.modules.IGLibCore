ba0ce4ba66c84d74b47f905876f9cf651c967124
Igor Grešovnik <gresovnik@gmail.com>
2026-03-10 14:40:13 +0100

    Corrected output from assertion in the previous test

--- tests/IGLib.Core.Tests/ConsoleAbstractionsTests/XUnitTestConsoleExamples_SwitchBuffering.cs
+++ tests/IGLib.Core.Tests/ConsoleAbstractionsTests/XUnitTestConsoleExamples_SwitchBuffering.cs
@@ -79,7 +79,8 @@ namespace IGLib.ConsoleAbstractions.Tests
                 IsConsoleOutputLineBuffered = true;
                 XUnitOutputConsole console = Console as XUnitOutputConsole;
                 Console.WriteLine($"The value of {nameof(IsConsoleOutputLineBuffered)} property: {IsConsoleOutputLineBuffered}");
-                IsConsoleOutputLineBuffered.Should().BeTrue(because: $"after switching to line buffered mode, the  should be truee");
+                IsConsoleOutputLineBuffered.Should().BeTrue(because: $"after switching to unbuffered mode, the {
+                    IsConsoleOutputLineBuffered} property should be false");
                 Console.WriteLine($"The value of {nameof(Console)}.{nameof(console.IsLineBuffered)} property: {console.IsLineBuffered}");
                 console.IsLineBuffered.Should().BeTrue(because: $"the value of the {nameof(console.IsLineBuffered)} propety on {
                     nameof(Console)} should be the same as the value of {nameof(IsConsoleOutputLineBuffered)} property on the test class");
