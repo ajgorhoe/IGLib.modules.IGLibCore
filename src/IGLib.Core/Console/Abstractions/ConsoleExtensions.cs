06e6d98f5e6dad0d6f25b9044acfb1c69e99c339
Igor Grešovnik <gresovnik@gmail.com>
2026-03-15 11:03:42 +0100

    Corrections - IConsole extensions - removed special methods for writing objects of type int (wrongly interferes with writing char parameter)

--- src/IGLib.Core/Console/Abstractions/ConsoleExtensions.cs
+++ src/IGLib.Core/Console/Abstractions/ConsoleExtensions.cs
@@ -16,9 +16,6 @@ namespace IGLib.ConsoleAbstractions.Extensions
         public static void WriteLine(this IConsole c, object? value)
             => c.WriteLine(value?.ToString());
 
-        public static void Write(this IConsole c, int value) => c.Write(value.ToString());
-        public static void WriteLine(this IConsole c, int value) => c.WriteLine(value.ToString());
-
         public static void Write(this IConsole c, string format, params object?[] args)
             => c.Write(string.Format(format, args));
 
