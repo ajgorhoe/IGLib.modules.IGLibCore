76c9c4c900a80ca4f8d13cc5fc50cff1baec9431
Igor Grešovnik <gresovnik@gmail.com>
2026-03-15 18:16:16 +0100

    Added ValidationSeverity enum

---
+++ src/IGLib.Core/Console/Validation/ValidationSeverity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGLib
+{
+
+    public enum ValidationSeverity
+    {
+        Warning = 1,
+        Error = 2
+    }
+
+
+}
