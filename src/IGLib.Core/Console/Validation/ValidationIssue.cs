aaf804a430d89ba47ebb19692765af4ddcfab1ce
Igor Grešovnik <gresovnik@gmail.com>
2026-03-15 20:40:22 +0100

    Updated definition

--- src/IGLib.Core/Console/Validation/ValidationIssue.cs
+++ src/IGLib.Core/Console/Validation/ValidationIssue.cs
@@ -8,15 +8,14 @@ namespace IGLib
     /// <summary>Contains information on a specific issue found during validation of a certain value.</summary>
     public sealed class ValidationIssue
     {
-        public ValidationIssue(ValidationSeverity severity, string message, Exception? exception = null)
-        {
-            if (string.IsNullOrWhiteSpace(message))
-            {
-                if (message is null)
-                    throw new ArgumentNullException(nameof(message));
-                throw new ArgumentException("Validation message must not be empty or whitespace.", nameof(message));
-            }
 
+        public ValidationIssue(ValidationSeverity severity, string message,
+            Exception? exception = null)
+        {
+            if (message is null)
+                throw new ArgumentNullException(nameof(message));
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentException("Validation message must not be empty or whitespace.", nameof(message));
             Severity = severity;
             Message = message;
             Exception = exception;
