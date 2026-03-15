7f08443116762dbfdbf3d98d35a0167445c55f50
Igor Grešovnik <gresovnik@gmail.com>
2026-03-15 11:42:54 +0100

    Added the ValidationResults class

---
+++ src/IGLib.Core/Console/ValidationResults.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGLib
+{
+    
+    /// <summary>Stores results of validation of an object provided externally (via user input, input/output,
+    /// via remote system, etc.).</summary>
+    /// <typeparam name="ValidatedType">Type of the object for which validation results are provided.</typeparam>
+    public class ValidationResults<ValidatedType>
+    {
+
+        public bool IsValid { get; protected set; }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    public class StringValidationResults: ValidationResults<string>
+    {  }
+
+}
