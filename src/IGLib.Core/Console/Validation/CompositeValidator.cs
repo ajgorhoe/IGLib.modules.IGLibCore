0dedd2a36522a899ffe86ed01e288bac050a27f2
Igor Grešovnik <gresovnik@gmail.com>
2026-03-15 23:55:03 +0100

    First implementattion of validation method

--- src/IGLib.Core/Console/Validation/CompositeValidator.cs
+++ src/IGLib.Core/Console/Validation/CompositeValidator.cs
@@ -20,6 +20,12 @@ namespace IGLib
         public void Validate(T value, ValidationResults results)
         {
             throw new NotImplementedException();
+            // ArgumentNullException.ThrowIfNull(results);
+
+            foreach (var validator in _validators)
+            {
+                validator.Validate(value, results);
+            }
         }
 
 
