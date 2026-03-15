69ddb722091a8db22e5123811e095d0b3b1d9ceb
Igor Grešovnik <gresovnik@gmail.com>
2026-03-16 00:04:54 +0100

    Updated CompositeValidator

--- src/IGLib.Core/Console/Validation/CompositeValidator.cs
+++ src/IGLib.Core/Console/Validation/CompositeValidator.cs
@@ -1,33 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
 namespace IGLib
 {
 
-    public sealed class CompositeValidator<T> // : IValidator<T>
+    public sealed class CompositeValidator<T> : IValidator<T>
     {
         private readonly IReadOnlyList<IValidator<T>> _validators;
 
         public CompositeValidator(IEnumerable<IValidator<T>> validators)
         {
-            throw new NotImplementedException();
             _validators = validators?.ToList()
                 ?? throw new ArgumentNullException(nameof(validators));
         }
 
         public void Validate(T value, ValidationResults results)
         {
-            // ArgumentNullException.ThrowIfNull(results);
-
+            if (results == null)
+            {
+                throw new ArgumentNullException(nameof(results));
+            }
             foreach (var validator in _validators)
             {
                 validator.Validate(value, results);
             }
         }
-
-
     }
 
 }
