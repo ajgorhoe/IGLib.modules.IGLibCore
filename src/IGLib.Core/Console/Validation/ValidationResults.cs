798a08ee8c5a771fc0dd5471c799674fec3f92f2
Igor Grešovnik <gresovnik@gmail.com>
2026-03-15 18:05:48 +0100

    Created a separate directory for validation-related tools, moved ValidationResults.cs

---
+++ src/IGLib.Core/Console/Validation/ValidationResults.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGLib
+{
+    
+    /// <summary>Stores results of validation of an object provided externally (via user input, input/output,
+    /// via remote system, etc.).</summary>
+    /// <typeparam name="ValidatedType">Type of the object for which validation results are provided.</typeparam>
+    /// <remarks>This class is not thread safe. It is not excepted that a single validation would be handled by multiple threads.</remarks>
+    public class ValidationResults<ValidatedType>
+    {
+
+        public virtual bool IsValid
+        {
+            get
+            {
+                return ErrorMessages.Count == 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>Contains validation error messages.</summary>
+        protected List<string> ErrorMessages { get; } = new();
+
+        protected List<string> WarningMessages { get; } = new();
+
+        /// <summary>Returns an aggregate string containing recorded validation error messages.
+        /// null is returned if there are no messages.</summary>
+        /// <param name="separator">A seprator string inserted between individual error messages; can be null.</param>
+        public virtual string? GetErrorsString(string separator) =>
+            ErrorMessages.Count == 0 ? null : string.Join(separator, ErrorMessages);
+
+        /// <summary>Returns an aggregate string containing recorded validation error messages, with individual
+        /// messages separated by new lines. null is returned if there are no messages.</summary>
+        public virtual string? GetErrorsString() => GetErrorsString(Environment.NewLine);
+
+        /// <summary>Returns an aggregate string containing recorded validation warning messages.
+        /// null is returned if there are no messages.</summary>
+        /// <param name="separator">A seprator string inserted between individual warning messages; can be null.</param>
+        public virtual string? GetWarningsString(string separator) =>
+            WarningMessages.Count == 0 ? null : string.Join(separator, WarningMessages);
+
+
+        /// <summary>Returns an aggregate string containing recorded validation warning messages, with individual
+        /// messages separated by new lines. null is returned if there are no messages.</summary>
+        public virtual string? GetWarningsString() => GetWarningsString(Environment.NewLine);
+
+        public void AddError(string errorMessage)
+        {
+            if (string.IsNullOrWhiteSpace(errorMessage))
+            {
+                if (errorMessage == null)
+                {
+                    throw new ArgumentNullException(nameof(errorMessage), "Validation error message may not be null.");
+                }
+                throw new ArgumentException("Validation error message may not consist purely of whitespace.", nameof(errorMessage));
+            }
+            ErrorMessages.Add(errorMessage);
+        }
+
+        public void AddWarning(string warningMessage)
+        {
+            if (string.IsNullOrWhiteSpace(warningMessage))
+            {
+                if (warningMessage == null)
+                {
+                    throw new ArgumentNullException(nameof(warningMessage), "Validation warning message may not be null.");
+                }
+                throw new ArgumentException("Validation warning message may not consist purely of whitespace.", nameof(warningMessage));
+            }
+            WarningMessages.Add(warningMessage);
+        }
+
+
+    }
+
+    /// <summary>Validation results for string objects.</summary>
+    public class StringValidationResults: ValidationResults<string>
+    {  }
+
+}
