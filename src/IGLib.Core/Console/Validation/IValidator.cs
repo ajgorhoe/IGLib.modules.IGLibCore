306aa64b881d6e1aee5abe7b3344b26700f0298b
Igor Grešovnik <gresovnik@gmail.com>
2026-03-15 20:53:54 +0100

    Added IValidator generic interface

---
+++ src/IGLib.Core/Console/Validation/IValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGLib
+{
+
+    /// <summary>Represent an object that performs validation of the value of the specified type (<typeparamref name="T"/>) 
+    /// and stores validation results in a <see cref="ValidationResults"/> objects.</summary>
+    /// <typeparam name="T">Type of values that the currentt validator validates.</typeparam>
+    public interface IValidator<in T>
+    {
+        void Validate(T value, ValidationResults results);
+    }
+
+}
