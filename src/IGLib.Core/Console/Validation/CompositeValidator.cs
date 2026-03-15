ca8788a8c9028e184b4fedac6465265658cc5126
Igor Grešovnik <gresovnik@gmail.com>
2026-03-15 23:49:45 +0100

    Composite validator - signature

--- src/IGLib.Core/Console/Validation/CompositeValidator.cs
+++ src/IGLib.Core/Console/Validation/CompositeValidator.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace IGLib
 {
 
-    
+    public sealed class CompositeValidator<T> // : IValidator<T>
+    {
+
+
+    }
 
 }
