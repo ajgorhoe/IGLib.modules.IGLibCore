1b6bd3a40a2ddbddc4ae87b9b09cc949dad3bb2b
Igor Grešovnik <gresovnik@gmail.com>
2026-03-16 23:28:09 +0100

    Added ParsableStringValidator

---
+++ src/IGLib.Core/Console/Validation/Implementations/StringParsing/ParsableStringValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace IGLib
+{
+
+    public class ParsableStringValidator<TTarget> : IValidator<string>
+    {
+        private static readonly IStringParser<TTarget> Parser = StringParserProvider.GetParser<TTarget>();
+
+        public ParsableStringValidator(string? errorMessage = null)
+        {
+            ErrorMessage = errorMessage ?? $"Value is not a valid {typeof(TTarget).Name}.";
+        }
+
+        public string ErrorMessage { get; }
+
+        public void Validate(string value, ValidationResults results)
+        {
+            // ArgumentNullException.ThrowIfNull(results);
+            if (results == null)
+            { throw new ArgumentNullException(nameof(results), "Object to store validation results may not be null."); }
+
+            if (value is null || !Parser.TryParse(value, out _))
+                results.AddError(ErrorMessage);
+        }
+    }
+
+}
