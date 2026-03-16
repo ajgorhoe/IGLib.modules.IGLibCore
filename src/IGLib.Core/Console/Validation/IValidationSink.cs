918227faefa66f54b1ce1e9be938b47f52230a05
Igor Grešovnik <gresovnik@gmail.com>
2026-03-16 01:46:41 +0100

    Added interface IValidationIssueSink - exposes a very limited part of the API of ValidationResult

---
+++ src/IGLib.Core/Console/Validation/IValidationSink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace IGLib
+{
+
+    /// <summary>The interface implemented by the <see cref="ValidationResult"/> class. It contains a very
+    /// reduced API from that class.</summary>
+    public interface IValidationIssueSink
+    {
+        void AddIssue(ValidationIssue issue);
+    }
+
+}
