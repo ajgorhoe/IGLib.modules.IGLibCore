73f60cef450d7e2f1273b20c7397dc0a3c57181b
Igor Grešovnik <gresovnik@gmail.com>
2026-03-16 23:34:45 +0100

    Added StringParserProvider stub

---
+++ src/IGLib.Core/Console/Validation/Implementations/StringParsing/StringParserProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace IGLib
+{
+
+
+    public static class StringParserProvider
+    {
+
+        public static IStringParser<T> GetParser<T>()
+        {
+            Type type = typeof(T);
+
+
+
+            throw new NotSupportedException($"Type {type.FullName} is not supported.");
+
+        }
+
+
+    }
+
+}
