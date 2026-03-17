3212872e7ecbfc3d08c21159054dec8d1b99baff
Igor Grešovnik <gresovnik@gmail.com>
2026-03-17 01:54:52 +0100

    Int32StringParser

--- src/IGLib.Core/Console/Validation/Implementations/StringParsing/Parsers.cs
+++ src/IGLib.Core/Console/Validation/Implementations/StringParsing/Parsers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -21,5 +22,20 @@ namespace IGLib
         public bool TryParse(string text, out bool value) =>
             bool.TryParse(text, out value);
     }
+
+
+    internal sealed class Int32StringParser : IStringParser<int>
+    {
+        private readonly IFormatProvider _formatProvider;
+
+        public Int32StringParser(IFormatProvider formatProvider)
+        {
+            _formatProvider = formatProvider;
+        }
+
+        public bool TryParse(string text, out int value) =>
+            int.TryParse(text, NumberStyles.Integer, _formatProvider, out value);
+    }
+
 
 }
