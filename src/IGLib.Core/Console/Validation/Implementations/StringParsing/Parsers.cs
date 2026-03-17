3cf6fb8fe0c2b8594617d92b3358dd1d10afc7bb
Igor Grešovnik <gresovnik@gmail.com>
2026-03-17 01:52:01 +0100

    Added file for IParsers implementations + IStringParser<bool>

---
+++ src/IGLib.Core/Console/Validation/Implementations/StringParsing/Parsers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace IGLib
+{
+
+    internal sealed class BooleanStringParser : IStringParser<bool>
+    {
+        public bool TryParse(string text, out bool value) =>
+            bool.TryParse(text, out value);
+    }
+
+}
