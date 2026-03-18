750839a5d7b2402fb7f88ee32f2014b70d14b621
Igor Grešovnik <gresovnik@gmail.com>
2026-03-18 08:52:59 +0100

    Added StringParserBase

---
+++ src/IGLib.Core/Console/Validation/Implementations/StringParsing/StringPsrserBase.cs
@@ -0,0 +1,16 @@
+﻿
+using IGLib;
+using System;
+
+internal abstract class StringParserBase<T> : IStringParser<T>
+{
+    protected StringParserBase(IFormatProvider? formatProvider = null)
+    {
+        FormatProvider = formatProvider ?? throw new ArgumentNullException(nameof(formatProvider));
+    }
+
+    protected IFormatProvider FormatProvider { get; }
+
+    public abstract bool TryParse(string text, out T value);
+}
+
