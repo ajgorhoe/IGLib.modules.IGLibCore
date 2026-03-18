628533c34824e35b0a0222c7123cde0d3a37fac8
Igor Grešovnik <gresovnik@gmail.com>
2026-03-18 16:03:06 +0100

    Properly implemented DefaultFormatProvider.cs

--- src/IGLib.Core/Console/Validation/DefaultFormatProvider/DefaultFormatProvider.cs
+++ src/IGLib.Core/Console/Validation/DefaultFormatProvider/DefaultFormatProvider.cs
@@ -7,24 +7,20 @@ using System.Text;
 namespace IGLib
 {
 
-    /// <summary>Provides the default format provider (object of type <see cref="IFormatProvider"/>) 
-    /// for cases where the format provider is not explicitly specified; e.g., in methods where
-    /// <see cref="IFormatProvider"/> is a parameter, but is not provided by the caller (parameter
-    /// is set to null).</summary>
-    /// <remarks><para>Usage example:</para>
-    /// <remarks>In constructors or methods that have <see cref="IFormatProvider"/> as parameter, also 
-    /// provide <see cref="IDefaultFormatProviderSelector?"/> as parameter, and default it to null.
-    /// Then, within the constructor, check if fhe format provider parameter is null, and if yes,
-    /// set it to what the default format provider selector's 
-    /// <see cref="DefaulltFormatProvider"/> returns. In applications, you can arrange for specific 
-    /// <see cref="IDefaultFormatProviderSelector"/> to be injected in all such constructors.</remarks>
-    /// </remarks>
-    public interface IDefaultFormatProviderSelector
+    public class DefaultFormatProviderSelector
     {
 
-        IFormatProvider DefaulltFormatProvider { get; }
+        /// <summary>Returns the global <see cref="IDefaultFormatProviderSelector"/> that is used to provide
+        /// the <see cref="IFormatProvider"/> when one is not specified, and also the the
+        /// <see cref="IDefaultFormatProviderSelector"/> is not specified.</summary>
+        public static IDefaultFormatProviderSelector Global { get; private set; } = 
+
+        public static IFormatProvider GetFormatProvider(IFormatProvider? specifiedProvider,
+            IDefaultFormatProviderSelector? selector)
+        {
+            return (specifiedProvider ?? (selector?? Global).DefaultFormatProvider);
+        }
 
     }
-
 
 }
