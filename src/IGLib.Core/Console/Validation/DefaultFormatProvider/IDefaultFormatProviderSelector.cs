33a494ba1c5d8c1816ed2ede935abfa4f94121b4
Igor Grešovnik <gresovnik@gmail.com>
2026-03-18 17:13:41 +0100

    Improved the form and comments

--- src/IGLib.Core/Console/Validation/DefaultFormatProvider/IDefaultFormatProviderSelector.cs
+++ src/IGLib.Core/Console/Validation/DefaultFormatProvider/IDefaultFormatProviderSelector.cs
@@ -1,29 +1,26 @@
 ﻿
 using System;
 
-namespace IGLib
+namespace IGLib;
+
+
+/// <summary>Provides the default format provider (object of type <see cref="IFormatProvider"/>) 
+/// for cases where the format provider is not explicitly specified; e.g., in methods where
+/// <see cref="IFormatProvider"/> is a parameter, but is not provided by the caller (parameter
+/// is set to null).</summary>
+/// <remarks><para>Usage example:</para>
+/// <remarks>In constructors or methods that have <see cref="IFormatProvider"/> as parameter, also 
+/// provide <see cref="IDefaultFormatProviderSelector?"/> as parameter, and default it to null.
+/// Then, within the constructor, check if fhe format provider parameter is null, and if yes,
+/// set it to what the default format provider selector's 
+/// <see cref="DefaulltFormatProvider"/> returns. In applications, you can arrange for specific 
+/// <see cref="IDefaultFormatProviderSelector"/> to be injected in all such constructors.</remarks>
+/// </remarks>
+public interface IDefaultFormatProviderSelector
 {
 
-    /// <summary>Provides the default format provider (object of type <see cref="IFormatProvider"/>) 
-    /// for cases where the format provider is not explicitly specified; e.g., in methods where
-    /// <see cref="IFormatProvider"/> is a parameter, but is not provided by the caller (parameter
-    /// is set to null).</summary>
-    /// <remarks><para>Usage example:</para>
-    /// <remarks>In constructors or methods that have <see cref="IFormatProvider"/> as parameter, also 
-    /// provide <see cref="IDefaultFormatProviderSelector?"/> as parameter, and default it to null.
-    /// Then, within the constructor, check if fhe format provider parameter is null, and if yes,
-    /// set it to what the default format provider selector's 
-    /// <see cref="DefaulltFormatProvider"/> returns. In applications, you can arrange for specific 
-    /// <see cref="IDefaultFormatProviderSelector"/> to be injected in all such constructors.</remarks>
-    /// </remarks>
-    public interface IDefaultFormatProviderSelector
-    {
-
-        /// <summary>Returns the <see cref="IFormatProvider"/> object that should be used as default,
-        /// when the <see cref="IFormatProvider"/> is not proveded for a specific purpose.</summary>
-        IFormatProvider DefaultFormatProvider { get; }
-
-    }
-
+    /// <summary>Returns the <see cref="IFormatProvider"/> object that is used as default,
+    /// when the <see cref="IFormatProvider"/> is not proveded for a specific purpose.</summary>
+    IFormatProvider DefaultFormatProvider { get; }
 
 }
