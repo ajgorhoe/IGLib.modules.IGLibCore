b0d6d048726fbc606463523c5766d8042cb73086
Igor Grešovnik <gresovnik@gmail.com>
2026-03-18 14:57:26 +0100

    Added DefaultFormatProvider/DefaultFormatProviderCurrentCulture class

---
+++ src/IGLib.Core/Console/Validation/DefaultFormatProvider/DefaultFormatProviderCurrentCulture.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace IGLib
+{
+
+    /// <summary>Provides the default format provider (object of type <see cref="IFormatProvider"/>) 
+    /// for cases where the format provider is not explicitly specified; e.g., in methods where
+    /// <see cref="IFormatProvider"/> is a parameter, but is not provided by the caller (parameter
+    /// is set to null).</summary>
+    /// <remarks>See also documentation for IDefaultFormatProviderSelector.</remarks>
+    public class DefaultFormatProviderSelectorCurrentCulture : IDefaultFormatProviderSelector
+    {
+
+        public IFormatProvider DefaultFormatProvider { get; } = CultureInfo.CurrentCulture;
+
+    }
+
+
+}
