b83608d6b3cc92f9ad09efa83137fb5c2d89b1e3
Igor Grešovnik <gresovnik@gmail.com>
2026-03-19 22:31:56 +0100

    Added FormatProviderDefaults, a simplified approach to select the default IFormatProvider when not specified

---
+++ src/IGLib.Core/Console/Validation/DefaultFormatProvider/Simple/FormatProviderDefaults.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace IGLib;
+
+public static class FormatProviderDefaults
+{
+    public static IFormatProvider Global { get; private set; } = CultureInfo.InvariantCulture;
+
+    public static void SetGlobal(IFormatProvider formatProvider)
+    {
+        if (formatProvider == null)
+            throw new ArgumentNullException(nameof(formatProvider));
+
+        Global = formatProvider;
+    }
+
+    public static IFormatProvider Resolve(IFormatProvider? specifiedFormatProvider)
+        => specifiedFormatProvider ?? Global;
+}
+
