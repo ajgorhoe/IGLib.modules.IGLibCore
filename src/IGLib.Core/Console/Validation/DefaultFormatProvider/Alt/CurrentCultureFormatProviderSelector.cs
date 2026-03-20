8fddc563bfd10ba1ffbb6dcb51837025bee21e46
Igor Grešovnik <gresovnik@gmail.com>
2026-03-20 22:45:35 +0100

    Alternative CurrentCultureFormatProviderSelector

---
+++ src/IGLib.Core/Console/Validation/DefaultFormatProvider/Alt/CurrentCultureFormatProviderSelector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace IGLib.Alt;
+
+public sealed class CurrentCultureFormatProviderSelector : IDefaultFormatProviderSelector
+{
+    public IFormatProvider DefaultFormatProvider => CultureInfo.CurrentCulture;
+}
+
