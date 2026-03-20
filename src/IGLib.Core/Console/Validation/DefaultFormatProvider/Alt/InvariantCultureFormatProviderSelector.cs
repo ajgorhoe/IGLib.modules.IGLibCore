2899f77c65a9f9e054adb0941c12ce3946d8fbbc
Igor Grešovnik <gresovnik@gmail.com>
2026-03-20 22:47:34 +0100

    Added invariant culture selector

---
+++ src/IGLib.Core/Console/Validation/DefaultFormatProvider/Alt/InvariantCultureFormatProviderSelector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace IGLib.Alt;
+
+public sealed class InvariantCultureFormatProviderSelector : IDefaultFormatProviderSelector
+{
+    public IFormatProvider DefaultFormatProvider => CultureInfo.InvariantCulture;
+
+}
