3405e4600ffd231bc5fd7b632368936f7a34d210
Igor Grešovnik <gresovnik@gmail.com>
2026-03-20 22:40:53 +0100

    Added alt. interface - IDefaultFormatProviderSelector

---
+++ src/IGLib.Core/Console/Validation/DefaultFormatProvider/Alt/IDefaultFormatProviderSelector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace IGLib.Alt;
+
+public interface IDefaultFormatProviderSelector
+{
+    IFormatProvider DefaultFormatProvider { get; }
+}
+
+
